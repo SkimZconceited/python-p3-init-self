60150674efdff35539dba4bbcf84dc46a08ba86f
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-12-03 22:03:01 +0300

    COmpleted assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def __init__(self, name, breed = 'Mutt') -> None:
+        self.name = name
+        self.breed = breed
+        # self.breed = None
+        # print(f'{name}')
+        pass
+
+    # def saves_self_breed(self, name, breed) -> None:
+    #     self.name = name
+    #     self.breed = breed
+        # print(f"{self.name} {self.breed}")
+        # pass
+    
+    
     pass
+
+fido = Dog("Fido")
+# fido.saves_self_breed("Fido", "Dalmatian")
+# fido = Dog("Fido", 'Dalmatian')
+
+print(fido.name)
+print(fido.breed)
+
+fido = Dog("Fido", "Dalmatian")
+print(fido.name)
+print(fido.breed)
+# print(fido.saves_self_breed("Fido", "Dalmatian"))
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Person:
+    def __init__(self, name) -> None:
+        self.name = name
+        pass
     pass
+
+guido = Person("Guido")
+
+print(guido.name)
